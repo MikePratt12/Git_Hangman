ce84dc63236fc7b38ad0c7b998169092e1908346
MikePratt12 <mike.n.pratt@gmail.com>
2025-04-30 16:08:41 +0100

    Made some changes

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@ import random
 
 # List of words for the game
 words = ["hangman", "python", "game", "programming", "openai"]
-
+Same changes as made in newbranch
 # Hangman graphics
 hangman_graphics = [
     '''
